cd28466379ebb585f343941361f430125f754b13
Sejinny <sejin97412@naver.com>
2023-05-12 09:55:29 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+    $('.main_img').slick({
+      autoplay : true,
+      dots : true,
+      pauseOnHover: false,
+    });
+  });
+
+
+  $('.new_slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay : true,
+  })
